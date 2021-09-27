03857bb457a077a463e279d43c252e89fdcd024c
TesfayDaniel <tesfaydaniel11@gmail.com>
2021-09-27 08:14:16 -0700

    My commit on 0x0E-structures_typedef project

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog{
-  char *name;
-  float age;
-  char *owner;
-    };
+struct dog {
+char *name;
+float age;
+char *owner;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 /**
