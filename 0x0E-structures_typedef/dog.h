4b7624462a140533f82b85af193db355bc101771
TesfayDaniel <tesfaydaniel11@gmail.com>
2021-09-27 18:07:18 -0700

    My commit on 0x0E-structures_typedef project

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,27 @@
-#ifndef DOG_H
-#define DOG_H
-struct dog {
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - such structure, wow
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+typedef struct dog
+{
 char *name;
 float age;
 char *owner;
-};
+}
+dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
+
 #endif
